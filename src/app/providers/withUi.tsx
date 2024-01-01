4aa3b39a274d09587f7da69b5796b61a229b0dd1
MezianeKhalil <khalilmez2000@gmail.com>
2024-01-01 17:40:47 +0100

    feat: add inter font family

--- src/app/providers/withUi.tsx
+++ src/app/providers/withUi.tsx
@@ -1,7 +1,15 @@
 import { ReactNode } from "react"
 
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import '@fontsource-variable/inter'
+
+const theme = extendTheme({
+    fonts: {
+        heading: `'Inter Variable', sans-serif`,
+        body: `'Inter Variable', sans-serif`,
+    },
+})
 
 export const withUi = (component: () => ReactNode) => () => {
-    return <ChakraProvider>{component()}</ChakraProvider>
+    return <ChakraProvider theme={theme}>{component()}</ChakraProvider>
 }
