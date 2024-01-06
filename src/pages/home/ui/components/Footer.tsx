15537449bc5f3fd0c789657d1a3497570a9dab68
MezianeKhalil <khalilmez2000@gmail.com>
2024-01-06 11:57:26 +0100

    fix: dark mode border color in footer

--- src/pages/home/ui/components/Footer.tsx
+++ src/pages/home/ui/components/Footer.tsx
@@ -1,8 +1,8 @@
-import { Box, Text, VStack } from "@chakra-ui/react"
+import { Box, Text, VStack, useColorModeValue } from "@chakra-ui/react"
 
 export default function Footer () {
     return (
-        <Box as='footer' py="6" borderTop="1px" borderColor="gray.200">
+        <Box as='footer' py="6" borderTop="1px" borderColor={useColorModeValue('gray.200', 'gray.700')}>
             <VStack>
                 <Text>©2024 GrandLine, All rights reserved.</Text>
             </VStack>
